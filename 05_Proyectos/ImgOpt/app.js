e9dcaad396ff1c8d48ed6abcca09df1e7cc80558
Kevin Moncada <kevinmonkada@gmail.com>
2023-12-12 16:40:52 -0500

    Refactor code for improved performance

---
+++ 05_Proyectos/ImgOpt/app.js
@@ -0,0 +1,16 @@
+import fse from "fs-extra";
+import imagemin from "imagemin";
+import imageminJpegtran from "imagemin-jpegtran";
+import imageminPngquant from "imagemin-pngquant";
+import imageminSvgo from "imagemin-svgo";
+import imageminWebp from "imagemin-webp";
+import imageminGifsicle from "imagemin-gifsicle";
+import sharp from "sharp";
+
+let inputFolder = "src";
+let outputFolder = "opt";
+let targetWidth = 1920;
+
+const processImage = async () => {};
+
+processImage();
